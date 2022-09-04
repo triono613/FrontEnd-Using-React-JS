c49bde79a0d9c78567f1de53604750f8da0766b2
komang  bp <komangbp@yahoo.com>
2022-09-04 21:29:19 +0700

    new push

---
+++ src/components/ProductList.js
@@ -0,0 +1,56 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
+
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        getProducts();
+    },[]);
+
+    const getProducts = async () => {
+        const products = await axios.get('http://localhost:8080/products');
+        setProducts(products.data);
+    }
+
+    const deleteProduct = async (id) =>{
+        await axios.delete(`http://localhost:8080/products/${id}`);
+        getProducts();
+    }
+
+    return (
+        <div>
+            <Link to="/add" className="button is-primary mt-5">Add New</Link>
+            <Table striped >
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { products.map((product, index) => (
+                        <tr key={product.id}>
+                            <td>{index + 1}</td>
+                            <td>{product.title}</td>
+                            <td>{product.price}</td>
+                            <td>
+                                <Link to={`/edit/${product.id}`} className="button is-small is-info">Edit</Link>
+                                <button onClick={() => deleteProduct(product.id)} className="button is-small is-danger">Delete</button>
+                            </td>
+                        </tr>
+                    )) }
+                    
+                </tbody>
+            </Table>
+        </div>
+    )
+}
+
+export default ProductList
